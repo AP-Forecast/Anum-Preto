c2b3c53ab6e79c7e576cdab0d557ca1d95a740db
Thiago Araujo <thiagorodrigues12372@gmail.com>
2024-11-28 10:02:56 -0300

    feat: now the data is correctly formated

--- src/main/java/API/Data.java
+++ src/main/java/API/Data.java
@@ -8,7 +8,7 @@ import java.util.ArrayList;
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class Data {
 
-    @JsonProperty("hourly")
+    @JsonProperty("daily")
     private DailyData daily;
 
     @JsonIgnoreProperties(ignoreUnknown = true)
@@ -22,14 +22,58 @@ public class Data {
         @JsonProperty("time")
         private ArrayList<String> time;
 
+        @JsonProperty("apparent_temperature_max")
+        private ArrayList<Double> apparentTempMax;
+
+        @JsonProperty("apparent_temperature_min")
+        private ArrayList<Double> apparentTempMin;
+
+        @JsonProperty("uv_index_max")
+        private ArrayList<Double> uvIndex;
+
+        @JsonProperty("precipitation_sum")
+        private ArrayList<Double> preciptation_sum;
+
+        @JsonProperty("rain_sum")
+        private ArrayList<Double> rain_sum;
+
+        @JsonProperty("wind_speed_10m_max")
+        private ArrayList<Double> maxWindSpeed;
+
+        @JsonProperty("wind_direction_10m_dominant")
+        private ArrayList<Double> windDirectionDegrees;
+
+        public ArrayList<Double> getTemperatureMax() {
+            return temperatureMax;
+        }
+
+        public ArrayList<Double> getTemperatureMin() {
+            return temperatureMin;
+        }
+
+        public ArrayList<String> getTime() {
+            return time;
+        }
+
         @Override
         public String toString() {
             return "DailyData{" +
                     "temperatureMax=" + temperatureMax +
                     ", temperatureMin=" + temperatureMin +
                     ", time=" + time +
+                    ", apparentTempMax=" + apparentTempMax +
+                    ", apparentTempMin=" + apparentTempMin +
+                    ", uvIndex=" + uvIndex +
+                    ", preciptation_sum=" + preciptation_sum +
+                    ", rain_sum=" + rain_sum +
+                    ", maxWindSpeed=" + maxWindSpeed +
+                    ", windDirectionDegrees=" + windDirectionDegrees +
                     '}';
         }
+    }
+
+    public DailyData getDaily() {
+        return daily;
     }
 
     @Override
